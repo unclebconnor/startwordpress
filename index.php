11784b1ead07a9fea166db5d2b1969e76e47f149
unclebconnor <bc.boxomail@gmail.com>
2018-02-07 12:19:56 -0800

    tutorial up to sidebar

--- index.php
+++ index.php
@@ -2,7 +2,11 @@
 
 	<div class="row">
 	  <div class="col-sm-8 blog-main">
-	  	<?php get_template_part('content', get_post_format() ); ?>
+	  	<?php 
+	  	if(have_posts()) : while( have_posts()) : the_post();
+	  		get_template_part('content', get_post_format() ); 
+	  	endwhile; endif;
+	  	?>
 	  </div><!-- /.blog-main -->
 	  <?php get_sidebar(); ?>
 	</div><!-- /.row -->
